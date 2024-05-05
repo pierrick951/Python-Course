da30e36b60c6610bf3f295b690a2500055e60c8f
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-05-05 19:00:45 -0400

    debut du cours

---
+++ 1-Variable/varibale.py
@@ -0,0 +1,22 @@
+# crée une varible  qui permet de stocker une données
+
+#exemple
+
+nom = 'marcel'
+age = 50
+
+print( age,nom) # on ecrit dans la console + age
+
+
+# on peut aditioner les varible en elle 
+
+var1 = 1
+var2 = 2 
+
+var3 = var1 + var2  #resulat 3
+
+
+#on peut aussi la multiplier
+
+teste = 'salut'
+print(teste*3) #return salutsalutsalut
