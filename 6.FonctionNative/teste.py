6e75a5db3639ede7ca011f9767aae497d948dd4b
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-05-09 17:33:56 -0400

    les fonction native en python

---
+++ 6.FonctionNative/teste.py
@@ -0,0 +1,24 @@
+#C'est quoi les Fonction Native ??
+
+"""
+Les fonctions natives en Python sont des fonctions intégrées directement dans le langage Python lui-même, ce qui signifie qu'elles sont disponibles dès que vous commencez à utiliser Python, sans avoir besoin d'importer des modules supplémentaires. Ces fonctions offrent un large éventail de fonctionnalités de base pour effectuer des opérations courantes, telles que la manipulation de chaînes de caractères, la conversion de types de données, l'interaction avec le système d'exploitation, etc.
+"""
+
+
+#  voici quelques exemples de fonction natives.abs
+"""
+print(): Affiche du texte ou des variables à la console.
+
+len(): Retourne la longueur d'une séquence (comme une liste ou une chaîne de caractères).
+
+type(): Retourne le type d'un objet.
+
+int(), float(), str(), bool(): Convertissent des valeurs en entiers, nombres à virgule flottante, chaînes de caractères et booléens respectivement.
+
+input(): Permet de saisir des données à partir du clavier.
+
+range(): Génère une séquence de nombres.
+
+sum(), max(), min(): Retournent la somme, le maximum ou le minimum d'une séquence donnée.
+
+open(), read(), write(), close(): Permettent la manipulation de fichier"""
